4f1e3204d76b4fda635dff2a763fb16d1ed0f38a
mi2428 <mi2428782020@gmail.com>
2018-04-13 22:02:28 +0900

    Update contents
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- js/members.js
+++ js/members.js
@@ -1,7 +1,8 @@
 $(document).ready(() => {
   const repository = "https://github.com/yamaoka-kitaguchi-lab/members/";
   const members_url = "https://raw.githubusercontent.com/yamaoka-kitaguchi-lab/members/master/members.json";
-  const past_members_url = "https://raw.githubusercontent.com/yamaoka-kitaguchi-lab/members/master/past_members.json";
+  const past_students_url = "https://raw.githubusercontent.com/yamaoka-kitaguchi-lab/members/master/past_students.json";
+  const past_exchange_students_url = "https://raw.githubusercontent.com/yamaoka-kitaguchi-lab/members/master/past_exchange_students.json";
 
   const build_members_list = (json) => {
     const thead = (group) => {
@@ -53,8 +54,100 @@ $(document).ready(() => {
     }
   };
 
-  const build_past_members_list = (json) => {
-    console.log(json);
+  const build_past_students_list = (json) => {
+    const thead = (group) => {
+      const label = group['label'];
+      return $('<thead>')
+        .append($('<tr>')
+          .append($('<th>').text(label['date']['ja']))
+          .append($('<th>').text(label['date']['en']))
+          .append($('<th>').text(label['name']['ja']))
+          .append($('<th>').text(label['name']['en']))
+          .append($('<th>').text(label['degree']['ja']))
+          .append($('<th>').text(label['degree']['en'])));
+    };
+
+    const tbody = (group) => {
+      const members = group['members'];
+      var tbody = $('<tbody>')
+      for (var idx in members) {
+        const member = members[idx];
+        var name_ja = member['name']['ja'];
+        var name_en = member['name']['en'];
+        if (member['url'] != undefined) {
+          name_ja = $('<a href="' + member['url'] + '">').text(name_ja);
+          name_en = $('<a href="' + member['url'] + '">').text(name_en);
+        }
+        tbody.append($('<tr>')
+          .append($('<td>').text(member['date']['ja']))
+          .append($('<td>').text(member['date']['en']))
+          .append($('<td>').append(name_ja))
+          .append($('<td>').append(name_en))
+          .append($('<td>').text(member['degree']['ja']))
+          .append($('<td>').text(member['degree']['en'])));
+      }
+      return tbody;
+    };
+
+    for (var idx in json) {
+      const group = json[idx];
+      const heading = group['role']['ja'] + " " + group['role']['en'];
+      $("#pastmembers")
+        .append($('<h3>').text(heading))
+        .append($('<small class="no-margin info">')
+          .append($('<span class="glyphicon glyphicon-envelope">'))
+          .append("account@net.ict.e.titech.ac.jp"))
+        .append($('<div class="table-responsive panel panel-body">')
+          .append($('<table class="table">')
+            .append(thead(group))
+            .append(tbody(group))));
+    }
+  };
+
+  const build_past_exchange_students_list = (json) => {
+    const thead = (group) => {
+      const label = group['label'];
+      return $('<thead>')
+        .append($('<tr>')
+          .append($('<th>').text(label['period']['ja']))
+          .append($('<th>').text(label['period']['en']))
+          .append($('<th>').text(label['name']['ja']))
+          .append($('<th>').text(label['name']['en']))
+          .append($('<th>').text(label['affiliation']['en']))
+          .append($('<th>').text(label['program']['en'])));
+    };
+
+    const tbody = (group) => {
+      const members = group['members'];
+      var tbody = $('<tbody>')
+      for (var idx in members) {
+        const member = members[idx];
+        var name_ja = member['name']['ja'];
+        var name_en = member['name']['en'];
+        if (member['url'] != undefined) {
+          name_ja = $('<a href="' + member['url'] + '">').text(name_ja);
+          name_en = $('<a href="' + member['url'] + '">').text(name_en);
+        }
+        tbody.append($('<tr>')
+          .append($('<td>').text(member['period']['ja']))
+          .append($('<td>').text(member['period']['en']))
+          .append($('<td>').append(name_ja))
+          .append($('<td>').append(name_en))
+          .append($('<td>').append(members['affiliation']['en']))
+          .append($('<td>').append(members['program']['en'])));
+      }
+      return tbody;
+    };
+
+    for (var idx in json) {
+      const group = json[idx];
+      const heading = group['role']['ja'] + " " + group['role']['en'];
+      $("#pastmembers")
+        .append($('<div class="table-responsive panel panel-body">')
+          .append($('<table class="table">')
+            .append(thead(group))
+            .append(tbody(group))));
+    }
   };
 
   const fetch_and_build = (url, builder) => {
@@ -101,5 +194,6 @@ $(document).ready(() => {
 
   $("#erroralert").empty();
   fetch_and_build(members_url, build_members_list);
-  fetch_and_build(past_members_url, build_past_members_list);
+  fetch_and_build(past_students_url, build_past_students_list);
+  // fetch_and_build(past_exchange_students_url, build_past_exchange_students_list);
 });
